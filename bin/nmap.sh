38448de5533e0036e4104740db31502e4c49b162
Yuta Yamate <11874627+yutayamate@users.noreply.github.com>
2020-06-10 23:52:28 +0900

    Update nmap.sh

--- bin/nmap.sh
+++ bin/nmap.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 OUTPUT=$(echo $1 | tr '/' '_')
-nmap -sS -sU -T4 -A -sC -oA $OUTPUT -v $1
-curl -O https://raw.githubusercontent.com/honze-net/nmap-bootstrap-xsl/master/nmap-bootstrap.xsl
+nmap -sS -sU -T4 -A -sC -oA $OUTPUT -v $1 && \
+curl -s -O https://raw.githubusercontent.com/honze-net/nmap-bootstrap-xsl/master/nmap-bootstrap.xsl && \
 xsltproc -o $OUTPUT.html nmap-bootstrap.xsl $OUTPUT.xml
