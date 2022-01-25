e101a7c7e0f42fad21740314deafb25af2ac5386
Yuta Yamate <11874627+yutayamate@users.noreply.github.com>
2022-01-25 11:42:04 +0900

    Update listbin.sh

--- bin/listbin.sh
+++ bin/listbin.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-echo "${PATH}" | awk '{cnt=split($0, x, ":"); for (i=1; i<=cnt; i++) print x[i]}' | grep "${HOME}" | xargs ls -1
+echo "${PATH}" | awk '{cnt=split($0, x, ":"); for (i=1; i<=cnt; i++) print x[i]}' | grep "${HOME}" | xargs ls -1 2> /dev/null
