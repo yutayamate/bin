8d8ea144b3128075499a4a5913767f7347124bb2
Yuta Yamate <11874627+yutayamate@users.noreply.github.com>
2022-03-16 01:26:08 +0900

    Update vmrun.sh

--- bin/vmrun.sh
+++ bin/vmrun.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-find "${HOME}/Virtual Machines.localized" -type f -name "*.vmx" | fzf -m --prompt "Select virtual machines to start > " | xargs -I{} vmrun -T fusion start {} nogui
+find "${HOME}/Virtual Machines.localized" -type f -name "*.vmx" | fzf -m --prompt "Select virtual machines to start > " | xargs -I{} vmrun -T fusion start "{}" nogui
