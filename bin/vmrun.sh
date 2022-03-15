0189438da7de26c2b5880f4671785eef764358d2
Yuta Yamate <11874627+yutayamate@users.noreply.github.com>
2022-03-15 20:39:18 +0900

    Update vmrun.sh and vmsuspend.sh

--- bin/vmrun.sh
+++ bin/vmrun.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-VM=$(find "${HOME}/Virtual Machines.localized" -type f -name "*.vmx" | fzf +m) && \
+VM=$(find "${HOME}/Virtual Machines.localized" -type f -name "*.vmx" | fzf +m --prompt "Select a virtual machine to start > ") && \
 vmrun -T fusion start "$(echo "${VM}")" nogui
--- bin/vmsuspend.sh
+++ bin/vmsuspend.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-VM=$(vmrun list | grep .vmx | fzf +m) && \
+VM=$(vmrun list | grep .vmx | fzf +m --prompt "Select a virtual machine to suspend > ") && \
 vmrun -T fusion suspend "$(echo "${VM}")"
