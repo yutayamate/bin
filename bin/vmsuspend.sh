7482927d9c4798b8346a556af64f3c760b1e9d2f
Yuta Yamate <11874627+yutayamate@users.noreply.github.com>
2022-03-16 01:24:41 +0900

    Update vmsuspend.sh

--- bin/vmsuspend.sh
+++ bin/vmsuspend.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-vmrun list | sed 1d | fzf -m --prompt "Select virtual machines to suspend > " | xargs vmrun -T fusion suspend
+vmrun list | sed 1d | fzf -m --prompt "Select virtual machines to suspend > " | xargs -I{} vmrun -T fusion suspend "{}"
