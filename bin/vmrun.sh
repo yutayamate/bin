d968279dc80f627c806e7530ad6467a38317e0e6
Yuta Yamate <11874627+yutayamate@users.noreply.github.com>
2022-03-15 12:21:23 +0900

    Update vmrun.sh

--- bin/vmrun.sh
+++ bin/vmrun.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
-
-VMS=$(find "${HOME}/Virtual Machines.localized" -type f -name "*.vmx")
-VM=$(echo "${VMS}" | fzf +m) && \
+VM=$(find "${HOME}/Virtual Machines.localized" -type f -name "*.vmx" | fzf +m) && \
 vmrun -T fusion start "$(echo "${VM}")" nogui
