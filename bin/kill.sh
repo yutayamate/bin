0c4c89987316253be6f76a9d77d12a3307c49bd8
Yuta Yamate <11874627+yutayamate@users.noreply.github.com>
2022-04-16 19:24:59 +0900

    Update kill.sh

--- bin/kill.sh
+++ bin/kill.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-ps aux | sed 1d | fzf -m --prompt "Select processes to be killed > " | awk "{ print \$2 }" | xargs kill -9
+ps aux | sed 1d | fzf -m --prompt "Select processes to be killed > " | awk "{ print \$2 }" | xargs sudo kill -9
