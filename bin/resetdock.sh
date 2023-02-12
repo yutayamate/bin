93b79eee04d91cc6047aa40205e5edcadba23e36
Yuta Yamate <11874627+yutayamate@users.noreply.github.com>
2023-02-12 15:41:21 +0900

    Update scripts for macOS

--- bin/resetdock.sh
+++ bin/resetdock.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-[ "$(uname)" == "Darwin" ] || { echo "Error: $(uname) is not supported." >&2; exit 1; }
+test "$(uname)" = "Darwin" || { echo "Error: $(uname) is not supported." >&2; exit 1; }
 defaults delete com.apple.dock && killall Dock
--- bin/resetlaunchpad.sh
+++ bin/resetlaunchpad.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-[ "$(uname)" == "Darwin" ] || { echo "Error: $(uname) is not supported." >&2; exit 1; }
+test "$(uname)" = "Darwin" || { echo "Error: $(uname) is not supported." >&2; exit 1; }
 defaults write com.apple.dock ResetLaunchPad -bool true && killall Dock
