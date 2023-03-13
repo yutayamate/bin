8818dc853807f3770709ead0135ec6ce520e2229
Yuta Yamate <11874627+yutayamate@users.noreply.github.com>
2023-03-14 00:56:57 +0900

    Update findwordlist.sh

--- bin/findwordlist.sh
+++ bin/findwordlist.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-WORDLISTS_DIRS="/usr/share/wordlists"
+WORDLISTS_DIR="/usr/share/wordlists"
 SECLISTS_DIR="/usr/share/seclists"
 command -v fzf > /dev/null 2>&1 || { echo 'Error: "fzf" is not installed.' >&2; exit 1; }
 find -L ${WORDLISTS_DIR} ${SECLISTS_DIR} -type f -not -iname '*README*' 2>/dev/null | sort | fzf --prompt "Select a wordlist file > "
