a784e664ae34488071f9b5ddd3509f6aa4b447b0
Yuta Yamate <11874627+yutayamate@users.noreply.github.com>
2023-03-14 16:47:41 +0900

    Add code.sh

--- bin/code.sh
+++ bin/code.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+WORKSPACE_DIR=${1:-${HOME}/workspace}
 command -v code > /dev/null 2>&1 || { echo 'Error: "code" is not installed.' >&2; exit 1; }
 command -v fzf > /dev/null 2>&1 || { echo 'Error: "fzf" is not installed.' >&2; exit 1; }
-find ${1:-${HOME}} -name ".git" -exec dirname {} \; 2>/dev/null | fzf -m --prompt "Select projects to open > " | xargs code
+find ${WORKSPACE_DIR} -name ".git" -exec dirname {} \; 2>/dev/null | fzf -m --prompt "Select projects to open > " | xargs code
