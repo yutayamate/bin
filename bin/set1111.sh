890443a28664b5f7f1ad5affc427ddd5be394330
Yuta Yamate <11874627+yutayamate@users.noreply.github.com>
2023-03-28 21:28:22 +0900

    Add set1111.sh

--- bin/set1111.sh
+++ bin/set1111.sh
@@ -4,5 +4,5 @@
 test "$(uname)" = "Darwin" || { echo "Error: $(uname) is not supported." >&2; exit 1; }
 command -v fzf > /dev/null 2>&1 || { echo 'Error: "fzf" is not installed.' >&2; exit 1; }
 DNS_SERVERS=(1.1.1.1 1.0.0.1 2606:4700:4700::1111 2606:4700:4700::1001)
-SERVICE=$(networksetup -listallnetworkservices | grep -v "*" | fzf -m --prompt "Select a network service to configure \"1.1.1.1\" DNS > ")
+SERVICE=$(networksetup -listallnetworkservices | grep -v '*' | fzf -m --prompt "Select a network service to configure \"1.1.1.1\" DNS > ")
 networksetup -setdnsservers "${SERVICE}" "${DNS_SERVERS[@]}"
