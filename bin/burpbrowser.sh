594700d287e5d9526125ee591820f1eab64f861f
Yuta Yamate <11874627+yutayamate@users.noreply.github.com>
2023-05-12 11:36:05 +0900

    Add burpbrowser.sh

--- bin/burpbrowser.sh
+++ bin/burpbrowser.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 test "$(uname)" = "Darwin" || { echo "Error: $(uname) is not supported." >&2; exit 1; }
 LOCATION=$(find "/Applications" -type d -path "*/Burp Suite*.app/*/Chromium.app")
-test -n "${LOCATION}" && open -a "${LOCATION}" || echo "Error: Burp's browser is not found."
+test -n "${LOCATION}" && open -a "${LOCATION}" --args --incognito || echo "Error: Burp's browser is not found."
