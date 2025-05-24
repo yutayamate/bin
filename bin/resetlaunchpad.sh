0542e69bc8390617611cda018608b77c37c42797
Yuta Yamate <11874627+yutayamate@users.noreply.github.com>
2025-05-24 16:51:49 +0900

    Update resetlaunchpad.sh

--- bin/resetlaunchpad.sh
+++ bin/resetlaunchpad.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 test "$(uname)" = "Darwin" || { echo "Error: $(uname) is not supported." >&2; exit 1; }
-defaults write com.apple.dock ResetLaunchPad -bool true && killall Dock
+# defaults write com.apple.dock ResetLaunchPad -bool true && killall Dock
+sudo find 2>/dev/null /private/var/folders/ -type d -name com.apple.dock.launchpad -exec rm -rf {} +; killall Dock
