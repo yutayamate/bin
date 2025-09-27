6a272d85f15ae140920459c67450dfc49ff107ed
Yuta Yamate <11874627+yutayamate@users.noreply.github.com>
2025-09-28 01:17:17 +0900

    Add containers

--- bin/yt-dlp.sh
+++ bin/yt-dlp.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 ARGS="$*"
 command -v docker > /dev/null 2>&1 || { echo 'Error: "docker" is not installed.' >&2; exit 1; }
-docker run --rm -v ${PWD}:/media tnk4on/yt-dlp:latest ${ARGS}
+docker run --rm -v ${PWD}:/media ghcr.io/yutayamate/bin/yt-dlp:latest ${ARGS}
