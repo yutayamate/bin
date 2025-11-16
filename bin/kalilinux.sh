23f757a6107c96344db43234fc74429b2706d0e3
Yuta Yamate <11874627+yutayamate@users.noreply.github.com>
2025-11-17 02:50:40 +0900

    Update kalilinux.sh

--- bin/kalilinux.sh
+++ bin/kalilinux.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 command -v docker > /dev/null 2>&1 || { echo 'Error: "docker" is not installed.' >&2; exit 1; }
-docker start kalilinux -i > /dev/null 2>&1 || docker run --name kalilinux -it kalilinux/kali-rolling:latest bash
+docker start kalilinux -i || docker run --name kalilinux -it kalilinux/kali-rolling:latest bash
